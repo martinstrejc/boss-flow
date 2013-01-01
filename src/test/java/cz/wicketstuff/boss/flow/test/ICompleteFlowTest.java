f9c12d923d3f3b23900f8ee9071e931c00b78d94
martin.strejc <devnull@localhost>
2013-01-01 01:33:47 +0100

    Test constatnts

---
+++ src/test/java/cz/wicketstuff/boss/flow/test/ICompleteFlowTest.java
@@ -0,0 +1,42 @@
+package cz.wicketstuff.boss.flow.test;
+
+/**
+ * Interface that define mixed strings from 'test-flow-complete.xml'
+ * for directly testing inside classes.
+ * 
+ * @author Martin Strejc
+ *
+ */
+public interface ICompleteFlowTest {
+
+	public static final String S0initialState = "S0initialState";
+	public static final String S1realState = "S1realState";
+	public static final String S2ifState = "S2ifState";
+	public static final String S3viewState = "S3viewState";
+	public static final String S4viewState = "S4viewState";
+	public static final String S5switchState = "S5switchState";
+	public static final String S6viewStateInitial = "S6viewStateInitial";
+	public static final String S8joinState = "S8joinState";
+	public static final String S9finalState = "S9finalState";
+	
+	public static final String T01 = "t01";
+	public static final String T12 = "t12";
+	public static final String T15 = "t15";
+	public static final String T23 = "t23";
+	public static final String T24 = "t24";
+	public static final String T38 = "t38";
+	public static final String T45 = "t45";
+	public static final String T52 = "t52";
+	public static final String T56 = "t56";
+	public static final String T58 = "t58";
+	public static final String T68 = "t68";
+	public static final String T78 = "t78";
+	public static final String T89 = "t89";
+	public static final String T96 = "t96";
+	
+	public static final String EX_isState2 = "isState2";
+	public static final String EX_switchS5 = "switchS5";
+	public static final String CASE_toViewS6 = "toViewS6";
+	public static final String CASE_toFinal = "toFinal";
+
+}
