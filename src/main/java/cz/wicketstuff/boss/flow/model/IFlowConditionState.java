019231107eaf8ff8427c4594d4441e91c5e3da58
martin.strejc <devnull@localhost>
2013-03-03 11:48:52 +0100

    Change license to Apache Commons 2.0

--- src/main/java/cz/wicketstuff/boss/flow/model/IFlowConditionState.java
+++ src/main/java/cz/wicketstuff/boss/flow/model/IFlowConditionState.java
@@ -1,18 +1,18 @@
 /*
- * Et netera, http://boss.etnetera.cz - Copyright (C) 2012 
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License (version 2.1) as published by the Free Software
- * Foundation.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * See the GNU Lesser General Public License for more details:
- * http://www.gnu.org/licenses/lgpl-3.0.txt
- * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package cz.wicketstuff.boss.flow.model;
 
