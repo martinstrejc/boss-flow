37f38ce1305c05a8ad3315d37ad54838db82fd8e
martin.strejc <devnull@localhost>
2013-05-04 00:01:54 +0200

    State hash

--- src/main/java/cz/wicketstuff/boss/flow/model/basic/FlowState.java
+++ src/main/java/cz/wicketstuff/boss/flow/model/basic/FlowState.java
@@ -188,6 +188,32 @@ public class FlowState implements IFlowState {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((stateName == null) ? 0 : stateName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FlowState other = (FlowState) obj;
+		if (stateName == null) {
+			if (other.stateName != null)
+				return false;
+		} else if (!stateName.equals(other.stateName))
+			return false;
+		return true;
+	}
+
+	@Override
 	protected void finalize() throws Throwable {
 		if(transitionsMap != null) {
 			transitionsMap.clear();
