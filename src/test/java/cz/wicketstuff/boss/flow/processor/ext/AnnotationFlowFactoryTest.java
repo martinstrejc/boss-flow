1c3c1edc6376c1ff7f6c65c0a5c5d853282a5ad3
martin.strejc <devnull@localhost>
2013-05-21 23:06:57 +0200

    Annotated Listeners test

--- src/test/java/cz/wicketstuff/boss/flow/processor/ext/AnnotationFlowFactoryTest.java
+++ src/test/java/cz/wicketstuff/boss/flow/processor/ext/AnnotationFlowFactoryTest.java
@@ -18,6 +18,7 @@ package cz.wicketstuff.boss.flow.processor.ext;
 
 import static org.junit.Assert.*;
 
+import java.beans.Transient;
 import java.io.Serializable;
 
 import org.junit.After;
@@ -26,11 +27,22 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import cz.wicketstuff.boss.flow.annotation.FlowConditionProcessor;
 import cz.wicketstuff.boss.flow.annotation.FlowEvents;
+import cz.wicketstuff.boss.flow.annotation.FlowStateValidation;
+import cz.wicketstuff.boss.flow.annotation.FlowSwitchProcessorExpression;
+import cz.wicketstuff.boss.flow.annotation.FlowTransitionEvent;
 import cz.wicketstuff.boss.flow.annotation.FlowEvents.FlowEvent;
+import cz.wicketstuff.boss.flow.annotation.FlowStateEvent;
+import cz.wicketstuff.boss.flow.annotation.FlowStateEvent.StateEvent;
+import cz.wicketstuff.boss.flow.annotation.FlowTransitionEvent.TransitionEvent;
 import cz.wicketstuff.boss.flow.model.IFlowCarter;
 import cz.wicketstuff.boss.flow.model.basic.FlowCarter;
+import cz.wicketstuff.boss.flow.model.basic.FlowState;
+import cz.wicketstuff.boss.flow.model.basic.FlowTransition;
 import cz.wicketstuff.boss.flow.processor.IFlowListener;
+import cz.wicketstuff.boss.flow.processor.IFlowStateChangeListener;
+import cz.wicketstuff.boss.flow.processor.IFlowTransitionChangeListener;
 
 /**
  * @author Martin Strejc
@@ -63,13 +75,20 @@ public class AnnotationFlowFactoryTest {
 		notify1 = false;
 		notify1 = false;		
 	}
+	
+	private IFlowCarter<String> createFlowCarter() {
+		FlowCarter<String> carter = new FlowCarter<String>(1L, new FlowState("testState"));
+		carter.setPreviousTransition(new FlowTransition(1, "previousTransition"));
+		carter.setNextTransition(new FlowTransition(2, "nextTransition"));
+		return carter;
+	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
 
 	@Test(expected=FlowAnnotationException.class)
-	public void testGetFlowListenersObjectExcp() throws FlowAnnotationException {
+	public void testGetFlowListenersObjectExcp1() throws FlowAnnotationException {
 		annotationFactory.getFlowListeners(new Serializable() {
 
 			private static final long serialVersionUID = 1L;
@@ -82,10 +101,24 @@ public class AnnotationFlowFactoryTest {
 		});		
 	}		
 
+	@Test(expected=FlowAnnotationException.class)
+	public void testGetFlowListenersObjectExcp2() throws FlowAnnotationException {
+		annotationFactory.getFlowListeners(new Serializable() {
+
+			private static final long serialVersionUID = 1L;
+		
+			@FlowEvents
+			public void notifier1(String payload) {
+				notify1 = true;
+			}
+			
+		});		
+	}		
+
 	@Test
 	public void testGetFlowListenersObject() throws FlowAnnotationException {		
 		IFlowListener<String> listener;
-		IFlowCarter<String> flowCarter = new FlowCarter<String>();
+		IFlowCarter<String> flowCarter = createFlowCarter();
 		listener = annotationFactory.getFlowListeners(new Serializable() {
 
 			private static final long serialVersionUID = 1L;
@@ -144,59 +177,269 @@ public class AnnotationFlowFactoryTest {
 
 	}
 
-	@Test
-	public void testGetStateChangeListenersObject() {
-		fail("Not yet implemented"); // TODO
+	@Test(expected=FlowAnnotationException.class)
+	public void testGetStateChangeListenersObjectExcp1() throws FlowAnnotationException {
+		annotationFactory.getStateChangeListeners(new Serializable() {
+
+			private static final long serialVersionUID = 1L;
+		
+			@FlowStateEvent
+			public void notifier1() {
+				notify1 = true;
+			}
+			
+		});
+	}
+
+	@Test(expected=FlowAnnotationException.class)
+	public void testGetStateChangeListenersObjectExcp2() throws FlowAnnotationException {
+		annotationFactory.getStateChangeListeners(new Serializable() {
+
+			private static final long serialVersionUID = 1L;
+		
+			@FlowStateEvent
+			public void notifier1(String payload) {
+				notify1 = true;
+			}
+			
+		});
 	}
 
 	@Test
-	public void testGetStateValidationListenersObject() {
+	public void testGetStateChangeListenersObject() throws FlowAnnotationException {
+		IFlowStateChangeListener<String> listener;
+		IFlowCarter<String> flowCarter = createFlowCarter();
+		listener = annotationFactory.getStateChangeListeners(new Serializable() {
+
+			private static final long serialVersionUID = 1L;
+		
+			@SuppressWarnings("unused")
+			public void notifier1(IFlowCarter<String> carter) {
+				notify1 = true;
+			}
+			
+		});
+		listener.onStateEntry(flowCarter);
+		assertFalse("Listener is not annotated.", notify1);
+		clearResults();
+
+		listener = annotationFactory.getStateChangeListeners(new Serializable() {
+
+			private static final long serialVersionUID = 1L;
+		
+			@FlowStateEvent(event=StateEvent.onStateEntry)
+			public void notifier1(IFlowCarter<String> carter) {
+				notify1 = true;
+			}
+
+			@FlowStateEvent(event=StateEvent.onStateLeaving)
+			public void notifier2(IFlowCarter<String> carter) {
+				notify2 = true;
+			}
+			
+		});
+		listener.onStateEntry(flowCarter);
+		assertTrue("Expected onStateEntry.", notify1);
+		assertFalse("UNExpected onStateLeaving.", notify2);
+		clearResults();
+		listener.onStateLeaving(flowCarter);
+		assertFalse("UNExpected onStateEntry.", notify1);
+		assertTrue("Expected onStateLeaving.", notify2);
+		clearResults();
+
+		listener = annotationFactory.getStateChangeListeners(new Serializable() {
+
+			private static final long serialVersionUID = 1L;
+		
+			@FlowEvents(event=FlowEvent.all)
+			public void notifier1(IFlowCarter<String> carter) {
+				notify1 = true;
+			}
+
+		});
+		listener.onStateEntry(flowCarter);
+		assertTrue("Expected onStateEntry.", notify1);
+		assertTrue("Expected onStateLeaving.", notify2);
+		clearResults();
+		listener.onStateLeaving(flowCarter);
+		assertTrue("Expected onStateEntry.", notify1);
+		assertTrue("Expected onStateLeaving.", notify2);
+
+	}
+
+	@Test(expected=FlowAnnotationException.class)
+	public void testGetStateValidationListenersObjectExcp1() throws FlowAnnotationException {
+		annotationFactory.getStateValidationListeners(new Serializable() {
+
+			private static final long serialVersionUID = 1L;
+		
+			@FlowStateValidation
+			public void notifier1() {
+				notify1 = true;
+			}
+			
+		});
+	}
+	
+	@Test
+	public void testGetStateValidationListenersObject() throws FlowAnnotationException {
 		fail("Not yet implemented"); // TODO
 	}
 
-	@Test
-	public void testGetTransitionChangeListenersObject() {
-		fail("Not yet implemented"); // TODO
+	@Test(expected=FlowAnnotationException.class)
+	public void testGetTransitionChangeListenersObjectExcp1() throws FlowAnnotationException {
+		annotationFactory.getTransitionChangeListeners(new Serializable() {
+
+			private static final long serialVersionUID = 1L;
+		
+			@FlowTransitionEvent
+			public void notifier1() {
+				notify1 = true;
+			}
+			
+		});
+	}
+
+	@Test(expected=FlowAnnotationException.class)
+	public void testGetTransitionChangeListenersObjectExcp2() throws FlowAnnotationException {
+		annotationFactory.getTransitionChangeListeners(new Serializable() {
+
+			private static final long serialVersionUID = 1L;
+		
+			@FlowTransitionEvent
+			public void notifier1(String payload) {
+				notify1 = true;
+			}
+			
+		});
 	}
 
 	@Test
-	public void testGetFlowConditionProcessorsObject() {
+	public void testGetTransitionChangeListenersObject() throws FlowAnnotationException {
+		IFlowTransitionChangeListener<String> listener;
+		IFlowCarter<String> flowCarter = createFlowCarter();
+		listener = annotationFactory.getTransitionChangeListeners(new Serializable() {
+
+			private static final long serialVersionUID = 1L;
+		
+			@SuppressWarnings("unused")
+			public void notifier1(IFlowCarter<String> carter) {
+				notify1 = true;
+			}
+			
+		});
+		listener.onTransitionStart(flowCarter);
+		assertFalse("Listener is not annotated.", notify1);
+		clearResults();
+
+		listener = annotationFactory.getTransitionChangeListeners(new Serializable() {
+
+			private static final long serialVersionUID = 1L;
+		
+			@FlowTransitionEvent(event=TransitionEvent.onTransitionStart)
+			public void notifier1(IFlowCarter<String> carter) {
+				notify1 = true;
+			}
+
+			@FlowTransitionEvent(event=TransitionEvent.onTransitionFinished)
+			public void notifier2(IFlowCarter<String> carter) {
+				notify2 = true;
+			}
+			
+		});
+		listener.onTransitionStart(flowCarter);
+		assertTrue("Expected onTransitionStart.", notify1);
+		assertFalse("UNExpected onTransitionFinished.", notify2);
+		clearResults();
+		listener.onTransitionFinished(flowCarter);
+		assertFalse("UNExpected onTransitionStart.", notify1);
+		assertTrue("Expected onTransitionFinished.", notify2);
+		clearResults();
+
+		listener = annotationFactory.getTransitionChangeListeners(new Serializable() {
+
+			private static final long serialVersionUID = 1L;
+		
+			@FlowTransitionEvent(event=TransitionEvent.all)
+			public void notifier1(IFlowCarter<String> carter) {
+				notify1 = true;
+			}
+
+		});
+		listener.onTransitionStart(flowCarter);
+		assertTrue("Expected onTransitionStart.", notify1);
+		assertTrue("Expected onTransitionFinished.", notify2);
+		clearResults();
+		listener.onTransitionFinished(flowCarter);
+		assertTrue("Expected onTransitionStart.", notify1);
+		assertTrue("Expected onTransitionFinished.", notify2);
+	}
+	
+	@Test(expected=FlowAnnotationException.class)
+	public void testGetFlowConditionProcessorsObjectExcp1() throws FlowAnnotationException {
+		annotationFactory.getFlowConditionProcessors(new Serializable() {
+
+			private static final long serialVersionUID = 1L;
+		
+			@FlowConditionProcessor
+			public void notifier1() {
+				notify1 = true;
+			}
+			
+		});
+	}
+
+	public void testGetFlowConditionProcessorsObject() throws FlowAnnotationException {
 		fail("Not yet implemented"); // TODO
 	}
 
-	@Test
-	public void testGetFlowSwitchProcessorsObject() {
-		fail("Not yet implemented"); // TODO
+	@Test(expected=FlowAnnotationException.class)
+	public void testGetFlowSwitchProcessorsObjectExcp1() throws FlowAnnotationException {
+		annotationFactory.getFlowSwitchProcessors(new Serializable() {
+
+			private static final long serialVersionUID = 1L;
+		
+			@FlowSwitchProcessorExpression
+			public void notifier1() {
+				notify1 = true;
+			}
+			
+		});
 	}
 
 	@Test
-	public void testFindMethodCandidates() {
+	public void testGetFlowSwitchProcessorsObject() throws FlowAnnotationException {
 		fail("Not yet implemented"); // TODO
 	}
-
-	@Test
-	public void testFindMethodConditionCandidates() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testFindMethodSwitchCandidates() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testCheckFlowMethod() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testCheckConditionMethod() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testCheckSwitchMethod() {
-		fail("Not yet implemented"); // TODO
-	}
+	
+//	@Test
+//	public void testFindMethodCandidates() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public void testFindMethodConditionCandidates() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public void testFindMethodSwitchCandidates() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public void testCheckFlowMethod() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public void testCheckConditionMethod() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public void testCheckSwitchMethod() {
+//		fail("Not yet implemented"); // TODO
+//	}
 
 }
