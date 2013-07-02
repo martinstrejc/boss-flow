65e55cedc8d31bc26cbc555cc74c0b243e166ffb
martin.strejc <devnull@localhost>
2013-07-02 20:25:55 +0200

    Speed test

--- src/test/java/cz/wicketstuff/boss/flow/dev/MatcherSpeedTest.java
+++ src/test/java/cz/wicketstuff/boss/flow/dev/MatcherSpeedTest.java
@@ -38,7 +38,16 @@ import org.slf4j.LoggerFactory;
 
 public class MatcherSpeedTest {
 
-	public static final long REPEATS = 100000000;
+//	private static final long REPEATS = 10000000;
+//	private static final long REPEATS = 1000000;
+//	private static final long REPEATS = 100000;
+//	private static final long REPEATS = 10000;
+	private static final long REPEATS = 1000;
+
+	
+	private static boolean USE_LOG_TEXTS = false;
+//	private static boolean USE_LOG_TEXTS = true;
+	
 	
 	@Rule 
 	public TestName testName = new TestName();
@@ -47,34 +56,77 @@ public class MatcherSpeedTest {
 	
 	private NumberFormat timestampFormatter = new DecimalFormat("###,###");
 	
-	private final Logger log = LoggerFactory.getLogger(getClass().getName());
+	private static final Logger log = LoggerFactory.getLogger(MatcherSpeedTest.class);
 	
 	@BeforeClass
 	public static void setUpBeforeClass() {
+		log.trace("USE_LOG_TEXTS = {}", USE_LOG_TEXTS);
+		log.trace("REPEATS = {}", REPEATS);
+		
 		regexStrings = new ArrayList<>(1000);
 		regexStrings.add("(Ab\\s+)");
 		regexStrings.add("Text.*");
 		regexStrings.add(".*");
 		regexStrings.add("\\w+12");
-		regexStrings.add("\\s+");
+		regexStrings.add("^[a-z0-9_-]{3,15}$");
+		regexStrings.add("^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+		regexStrings.add("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$");
+		regexStrings.add("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})");
+		regexStrings.add("([^\\s]+(\\.(?i)(jpg|png|gif|bmp))$)");
+		regexStrings.add("^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|" +
+				"25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
+		regexStrings.add("(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|1[012])/((19|20)\\d\\d)");
+		regexStrings.add("<(\"[^\"]*\"|'[^']*'|[^'\">])*>");
 		
 		comparedStrings = new ArrayList<>(1000);
 		comparedStrings.add("Text12");
-		comparedStrings.add("cosi12");
-		comparedStrings.add("Nejaky nahodny text");
+		comparedStrings.add("fake12");
+		comparedStrings.add("Any random text");
 		comparedStrings.add(" \t\r\n");
-		
+		comparedStrings.add(" \t\r\n not only white spaces \t\r\n ");
+		comparedStrings.add("test@wicketstuff.cz");
+		comparedStrings.add("test@wicket@stuff.cz");
+		comparedStrings.add("<invalid<tag>");
+		comparedStrings.add("<a href=\"anywehere\">");
+		comparedStrings.add("2013/07/23");
+		if(USE_LOG_TEXTS) {
+			comparedStrings.add("CorrectPassword#1234");
+			comparedStrings.add("Exmaple text: Regular expression is an art of the programing, " +
+					"it’s hard to debug , learn and understand, " +
+					"but the powerful features are still " +
+					"attract many developers to code regular expression. " +
+					"Let’s explore the following 10 practical regular expression ~ enjoy :)");
+			comparedStrings.add("Exmaple text: 2013/07/23Pattern Class: A Pattern object is a compiled representation " +
+					"of a regular expression. The Pattern class provides no public constructors. " +
+					"To create a pattern, you must first invoke one of its public static compile methods, " +
+					"which will then return a Pattern object. These methods accept " +
+					"a regular expression as the first argument. " +
+					"Matcher Class: A Matcher object is the engine that interprets " +
+					"the pattern and performs match operations against an input string. " +
+					"Like the Pattern class, Matcher defines no public constructors. " +
+					"You obtain a Matcher object by invoking the matcher method on a Pattern object. " +
+					"PatternSyntaxException: A PatternSyntaxException object is an unchecked exception " +
+					"that indicates a syntax error in a regular expression pattern. ");
+			comparedStrings.add("Exmaple text: Regular expression is an art of the programing, " +
+					"it’s hard to debug , learn and understand, " +
+					"but the powerful features are still " +
+					"attract many <a href=\"developers\">developers</a> to code regular expression. " +
+					"Let’s explore the following 10 practical regular expression ~ enjoy :)");
+			comparedStrings.add("Exmaple text: Regular expression is an art of the programing, " +
+					"it’s hard to debug , learn and understand, " +
+					"but the powerful features are still " +
+					"attract many to code regular expression. " +
+					"Let’s explore the following 10 practical regular expression ~ enjoy :) " +
+					"myemail@mydomain.com");			
+		}
 		
 		regexPatterns = new ArrayList<>(regexStrings.size());
+		matcherCache = new ArrayList<>(regexPatterns.size());
 		for(String s : regexStrings) {
 			regexPatterns.add(Pattern.compile(s));
+			matcherCache.add(new HashMap<String, Matcher>(16));
 		}
 
-		matcherCache = new HashMap<>(regexPatterns.size());
-		for(Pattern p : regexPatterns) {
-			matcherCache.put(p, new HashMap<String, Matcher>(16));
-		}
-		
 	}
 
 	@AfterClass
@@ -93,7 +145,7 @@ public class MatcherSpeedTest {
 	
 	@Before
 	public void setUp() {
-		log.trace("Running {}", testName.getMethodName());
+		// log.trace("Running {}", testName.getMethodName());
 		startDate = new Date();
 	}
 
@@ -101,7 +153,7 @@ public class MatcherSpeedTest {
 	public void tearDown() {
 		Date now = new Date();
 		long delta = now.getTime() - startDate.getTime();
-		log.trace("{} runs {} ms", testName.getMethodName(), timestampFormatter.format(delta));
+		log.trace("{} has been running {} ms", testName.getMethodName(), timestampFormatter.format(delta));
 	}
 	
 	public MatcherSpeedTest() {
@@ -125,8 +177,9 @@ public class MatcherSpeedTest {
 	@Test
 	public void testCachedPatternMatcher() throws Exception {
 		for(long l = 0; l < REPEATS; l++) {
-			Pattern p = getRandomPattern();
-			Map<String, Matcher> matcherMap = matcherCache.get(p);
+			int index = rand(regexPatterns.size());
+			Pattern p = regexPatterns.get(index);
+			Map<String, Matcher> matcherMap = matcherCache.get(index);
 			String text = getRandomRegex();
 			Matcher matcher = matcherMap.get(text);
 			if(matcher == null) {
@@ -156,7 +209,7 @@ public class MatcherSpeedTest {
 	private static List<String> regexStrings;
 	private static List<Pattern> regexPatterns;
 	private static List<String> comparedStrings;
-	private static Map<Pattern, Map<String, Matcher>> matcherCache;
+	private static List<Map<String, Matcher>> matcherCache;
 	
 	
 
